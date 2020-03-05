7e0cc243e265609134e21bc977f9bbec853e30e4
Alexandra <alexandrajn17@gmail.com>
2020-03-05 13:29:51 +0000

    x & y axis set

---
+++ main.js
@@ -0,0 +1,25 @@
+//Chart Options
+const options = {
+  chart: {
+    height: 450,
+    width: "100%",
+    type: "bar",
+    background: "#f4f4f4",
+    foreColor: "#333"
+  },
+  series: [
+    {
+      name: "Meditation Minutes",
+      data: [2, 5, 14, 27, 9, 11, 34]
+    }
+  ],
+  xaxis: {
+    categories: ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"]
+  }
+};
+
+//Initialise Chart
+const chart = new ApexCharts(document.querySelector("#chart"), options); //takes in a selector that we want to output our data in
+
+//Render Chart
+chart.render();
